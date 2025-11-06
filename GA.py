15c571e920e7b62c2f7c2e83e00cb23f3663ec16
onLuc <lucvdriel01@gmail.com>
2025-11-06 20:40:58 +0100

    framework voor GA, maar het werkt inconsistent ondanks mn random seeds

--- GA.py
+++ GA.py
@@ -5,31 +5,49 @@ import numpy as np
 # https://pypi.org/project/ioh/
 import ioh
 from ioh import get_problem, logger, ProblemClass
+import random
 
+np.random.seed(1)
+random.seed(1)
 budget = 5000
+pop_size = 100
+dim = 50
 
-# To make your results reproducible (not required by the assignment), you could set the random seed by
-# `np.random.seed(some integer, e.g., 42)`
+def mutate(pop):
+    pass
+
+def crossover(pop):
+    pass
+
+def selection(pop):
+    pass
 
 def studentnumber1_studentnumber2_GA(problem: ioh.problem.PBO) -> None:
-    # initial_pop = ... make sure you randomly create the first population
-
+    pop = [[random.choice([0, 1]) for _ in range(dim)] for _ in range(pop_size)]
+    pop_matrix = np.array(pop, dtype=int)
+    trans_matrix = 2 * pop_matrix - 1
+    print(trans_matrix)
     # `problem.state.evaluations` counts the number of function evaluation automatically,
     # which is incremented by 1 whenever you call `problem(x)`.
     # You could also maintain a counter of function evaluations if you prefer.
     while problem.state.evaluations < budget:
+        # pop = mutate(pop)
+        # pop = crossover(pop)
+        # pop = selection(pop)
         # please implement the mutation, crossover, selection here
-        # .....
-        # this is how you evaluate one solution `x`
-        # f = problem(x)
-    # no return value needed
-        pass
+
+        # f = problem(trans_matrix)
+        fitness_values = [problem(ind) for ind in trans_matrix]
+        print(fitness_values)
 
 
 def create_problem(dimension: int, fid: int) -> Tuple[ioh.problem.PBO, ioh.logger.Analyzer]:
     # Declaration of problems to be tested.
     problem = get_problem(fid, dimension=dimension, instance=1, problem_class=ProblemClass.PBO)
 
+    triggers = [
+        ioh.logger.trigger.Each(1)
+    ]
     # Create default logger compatible with IOHanalyzer
     # `root` indicates where the output files are stored.
     # `folder_name` is the name of the folder containing all output. You should compress the folder 'run' and upload it to IOHanalyzer.
@@ -38,6 +56,7 @@ def create_problem(dimension: int, fid: int) -> Tuple[ioh.problem.PBO, ioh.logge
         folder_name="run",  # the folder name to which the raw performance data will be stored
         algorithm_name="genetic_algorithm",  # name of your algorithm
         algorithm_info="Practical assignment of the EA course",
+        triggers=triggers,
     )
     # attach the logger to the problem
     problem.attach_logger(l)
@@ -47,15 +66,16 @@ def create_problem(dimension: int, fid: int) -> Tuple[ioh.problem.PBO, ioh.logge
 if __name__ == "__main__":
     # this how you run your algorithm with 20 repetitions/independent run
     # create the LABS problem and the data logger
-    F18, _logger = create_problem(dimension=50, fid=18)
-    for run in range(20): 
+    F18, _logger = create_problem(dimension=dim, fid=18)
+    # for run in range(20):
+    for run in range(1):
         studentnumber1_studentnumber2_GA(F18)
         F18.reset() # it is necessary to reset the problem after each independent run
     _logger.close() # after all runs, it is necessary to close the logger to make sure all data are written to the folder
 
     # create the N-Queens problem and the data logger
-    F23, _logger = create_problem(dimension=49, fid=23)
-    for run in range(20): 
-        studentnumber1_studentnumber2_GA(F23)
-        F23.reset()
-    _logger.close()
+    # F23, _logger = create_problem(dimension=49, fid=23)
+    # for run in range(20):
+    #     studentnumber1_studentnumber2_GA(F23)
+    #     F23.reset()
+    # _logger.close()
