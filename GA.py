5e35c6306edeafb5b255eec589af68e58ea663e7
onLuc <lucvdriel01@gmail.com>
2025-11-06 19:04:30 +0100

    original base

---
+++ GA.py
@@ -0,0 +1,61 @@
+from typing import Tuple 
+import numpy as np
+# you need to install this package `ioh`. Please see documentations here: 
+# https://iohprofiler.github.io/IOHexp/ and
+# https://pypi.org/project/ioh/
+import ioh
+from ioh import get_problem, logger, ProblemClass
+
+budget = 5000
+
+# To make your results reproducible (not required by the assignment), you could set the random seed by
+# `np.random.seed(some integer, e.g., 42)`
+
+def studentnumber1_studentnumber2_GA(problem: ioh.problem.PBO) -> None:
+    # initial_pop = ... make sure you randomly create the first population
+
+    # `problem.state.evaluations` counts the number of function evaluation automatically,
+    # which is incremented by 1 whenever you call `problem(x)`.
+    # You could also maintain a counter of function evaluations if you prefer.
+    while problem.state.evaluations < budget:
+        # please implement the mutation, crossover, selection here
+        # .....
+        # this is how you evaluate one solution `x`
+        # f = problem(x)
+    # no return value needed
+        pass
+
+
+def create_problem(dimension: int, fid: int) -> Tuple[ioh.problem.PBO, ioh.logger.Analyzer]:
+    # Declaration of problems to be tested.
+    problem = get_problem(fid, dimension=dimension, instance=1, problem_class=ProblemClass.PBO)
+
+    # Create default logger compatible with IOHanalyzer
+    # `root` indicates where the output files are stored.
+    # `folder_name` is the name of the folder containing all output. You should compress the folder 'run' and upload it to IOHanalyzer.
+    l = logger.Analyzer(
+        root="data",  # the working directory in which a folder named `folder_name` (the next argument) will be created to store data
+        folder_name="run",  # the folder name to which the raw performance data will be stored
+        algorithm_name="genetic_algorithm",  # name of your algorithm
+        algorithm_info="Practical assignment of the EA course",
+    )
+    # attach the logger to the problem
+    problem.attach_logger(l)
+    return problem, l
+
+
+if __name__ == "__main__":
+    # this how you run your algorithm with 20 repetitions/independent run
+    # create the LABS problem and the data logger
+    F18, _logger = create_problem(dimension=50, fid=18)
+    for run in range(20): 
+        studentnumber1_studentnumber2_GA(F18)
+        F18.reset() # it is necessary to reset the problem after each independent run
+    _logger.close() # after all runs, it is necessary to close the logger to make sure all data are written to the folder
+
+    # create the N-Queens problem and the data logger
+    F23, _logger = create_problem(dimension=49, fid=23)
+    for run in range(20): 
+        studentnumber1_studentnumber2_GA(F23)
+        F23.reset()
+    _logger.close()
