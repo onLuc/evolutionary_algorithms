614d09c95ea0af577d8d1465c869c8fcf7674093
onLuc <lucvdriel01@gmail.com>
2025-11-20 15:57:52 +0100

    Fundament staat. Heb queens probleem nog niet helemaal kunnen runnen, omdat de functie denk ik lang duurt om te testen op problem, maar geen error gekregen, dus denk dat de code daar ook op werkt.

--- GA.py
+++ GA.py
@@ -1,41 +1,65 @@
-from typing import Tuple 
+from typing import Tuple
 import numpy as np
-# you need to install this package `ioh`. Please see documentations here: 
+# you need to install this package `ioh`. Please see documentations here:
 # https://iohprofiler.github.io/IOHexp/ and
 # https://pypi.org/project/ioh/
 import ioh
 from ioh import get_problem, logger, ProblemClass
 import random
+np.random.seed(1)
+random.seed(1)
 
 from numpy.ma.core import append
 from numpy.matlib import empty
 
-np.random.seed(1)
-random.seed(1)
 budget = 10000
 pop_size = 100
 dim = 50
 
 def selection(pop, fitness):
-    # print(pop)
-    # print(fitness)
     sorted_pairs = sorted(zip(pop, fitness), key=lambda x: x[1], reverse=True)
     new_pop, _ = zip(*sorted_pairs)
-    new_pop = new_pop[:51]
+    new_pop = new_pop[:pop_size] # Must always be even for crossover
+
+    return list(new_pop)
+
+def crossover(pop, type_crossover="index", n=3):
+    new_pop = []
+    while pop:
+        a = random.sample(range(len(pop)),1)
+        ind1 = pop.pop(a[0])
+        b = random.sample(range(len(pop)),1)
+        ind2 = pop.pop(b[0])
+        # Adding originals to new pop
+        new_pop.append(ind1)
+        new_pop.append(ind2)
+        new_ind1 = []
+        new_ind2 = []
+        if type_crossover == "index":
+            indices = sorted(random.sample(range(1, len(ind1)), n))
+            swap = False
+            for enum, bit in enumerate(zip(ind1, ind2)):
+                if enum in indices:
+                    swap = not swap
+                new_ind1.append(bit[1] if swap else bit[0])
+                new_ind2.append(bit[0] if swap else bit[1])
+        elif type_crossover == "uniform":
+            pass
+        else:
+            raise TypeError("Crossover type must be index or uniform")
+        new_pop.append(new_ind1)
+        new_pop.append(new_ind2)
 
     return new_pop
 
-def crossover(pop):
-    pass
 
-def mutate(pop):
-    mutation_rate = 0.05
+def mutate(pop, mutation_rate):
     new_pop = []
     for ind in pop:
         new_ind = []
         for i in ind:
             if random.random() < mutation_rate:
-                new_ind.append(i+1%2)
+                new_ind.append((i+1)%2)
             else:
                 new_ind.append(i)
         new_pop.append(new_ind)
@@ -48,22 +72,16 @@ def studentnumber1_studentnumber2_GA(problem: ioh.problem.PBO) -> None:
     fitness = []
     while problem.state.evaluations < budget:
         if fitness:
-            # print(fitness)
+            pop = crossover(pop, "index", 3)
+            pop = mutate(pop, mutation_rate=0.0075)
             pop = selection(pop, fitness)
-            # pop = crossover(pop)
-            pop = mutate(pop)
-        # please implement the mutation, crossover, selection here
-        trans_list = [[2 * i - 1 for i in ind] for ind in pop]
-        fitness = problem(trans_list)
+        fitness = problem(pop)
 
 
 def create_problem(dimension: int, fid: int) -> Tuple[ioh.problem.PBO, ioh.logger.Analyzer]:
     # Declaration of problems to be tested.
     problem = get_problem(fid, dimension=dimension, instance=1, problem_class=ProblemClass.PBO)
 
-    triggers = [
-        ioh.logger.trigger.Each(1)
-    ]
     # Create default logger compatible with IOHanalyzer
     # `root` indicates where the output files are stored.
     # `folder_name` is the name of the folder containing all output. You should compress the folder 'run' and upload it to IOHanalyzer.
@@ -72,7 +90,6 @@ def create_problem(dimension: int, fid: int) -> Tuple[ioh.problem.PBO, ioh.logge
         folder_name="run",  # the folder name to which the raw performance data will be stored
         algorithm_name="genetic_algorithm",  # name of your algorithm
         algorithm_info="Practical assignment of the EA course",
-        triggers=triggers,
     )
     # attach the logger to the problem
     problem.attach_logger(l)
@@ -83,22 +100,14 @@ if __name__ == "__main__":
     # this how you run your algorithm with 20 repetitions/independent run
     # create the LABS problem and the data logger
     F18, _logger = create_problem(dimension=dim, fid=18)
-    # for run in range(20):
-
     for run in range(20):
-        np.random.seed(1)
-        random.seed(1)
-        F18.reset()
         studentnumber1_studentnumber2_GA(F18)
-
-    # for run in range(1):
-    #     studentnumber1_studentnumber2_GA(F18)
-    #     F18.reset() # it is necessary to reset the problem after each independent run
-    # _logger.close() # after all runs, it is necessary to close the logger to make sure all data are written to the folder
+        F18.reset()
+    _logger.close()
 
     # create the N-Queens problem and the data logger
-    # F23, _logger = create_problem(dimension=49, fid=23)
-    # for run in range(20):
-    #     studentnumber1_studentnumber2_GA(F23)
-    #     F23.reset()
-    # _logger.close()
+    F23, _logger = create_problem(dimension=49, fid=23)
+    for run in range(20):
+        studentnumber1_studentnumber2_GA(F23)
+        F23.reset()
+    _logger.close()
